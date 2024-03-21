4898f354051ce4af1d58a204dd7ffda89ee057f4
ConeCut <enachescucosmin2016@gmail.com>
2024-03-21 13:36:12 +0100

    database and documentation

--- database/ticket_db/ticket.sql
+++ database/ticket_db/ticket.sql
@@ -6,6 +6,9 @@ create table ticket_db.ticket
     ticket_type   varchar(255) null,
     ticket_nr     int          not null,
     ticket_status varchar(255) not null,
+    user_email varchar(255) not null,
+    user_username varchar(255) not null,
+
     constraint user_id_fk
         foreign key (user_id) references ticket_db.users (id)
 );
